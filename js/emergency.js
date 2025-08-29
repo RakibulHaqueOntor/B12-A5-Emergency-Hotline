ec00a332e8fb30fd379125edbe306b1d6d70d5c1
RakibulHaqueOntor <rakibulhaqueontor@gmail.com>
2025-08-29 12:35:23 +0600

    responsive work remaining

--- js/emergency.js
+++ js/emergency.js
@@ -19,16 +19,16 @@ for(let btn of buttons){
       coinLeft = total_coin-20;
       document.getElementById('coins').innerText = coinLeft;
       console.log(total_coin);
-      const cart = document.getElementById('right_div')
+      const cart = document.getElementById('cart_container')
       const newCart = document.createElement('div');
       newCart.innerHTML=
-      `<div class="call h-[86px] w-[352px] rounded-[8px] flex justify-between       items-center bg-[#fafafa] mt-[16px]">
+      `<div class="call h-[86px] w-[352px] rounded-[8px] flex justify-between  items-center bg-[#fafafa] mt-[16px]">
             <div>
-              <p>${title}</p>
-              <p>${phone_number}</p>
+              <p class="text-[18px] font-medium">${title}</p>
+              <p class="text-[18px] text-gray-500">${phone_number}</p>
             </div>
             <div>
-              <p>${present_time}</p>
+              <p class = "text-[18px] font-medium">${present_time}</p>
             </div>
           </div>`;
       cart.appendChild(newCart);  
@@ -40,8 +40,11 @@ for(let btn of buttons){
 )}
     const clear = document.getElementById('clear_btn') 
     clear.addEventListener('click',function(){
-    console.log('button clicked');
-    document.getElementsByClassName('call').display = 'hidden';
+      // console.log("clicked")
+    // const newCart = document.createElement('div');
+    //   newCart.innerHTML="";
+    const cart = document.getElementById('cart_container')
+    cart.innerHTML = "";
     
   })
   const copyS = document.getElementsByClassName('copy_btn')
@@ -49,6 +52,7 @@ for(let cpy of copyS){
   cpy.addEventListener('click',function(){
     
     const number = cpy.parentNode.parentNode.children[2].children[0].innerText
+    navigator.clipboard.writeText(number)
     console.log(number)
     alert("নম্বরটি কপি হয়েছে " + number);
     const total_copy = parseInt(cpy.parentNode.parentNode.parentNode.parentNode.parentNode.children[0].children[0].children[1].children[2].children[0].childNodes[0].innerText);
